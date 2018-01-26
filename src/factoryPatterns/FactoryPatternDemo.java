2824a188ea34d380003d5251cfa6cc7d2de6de81
MJCoderMJCoder <598157378@qq.com>
2018-01-26 14:26:08 +0800

    各种经典的设计模式总结 Signed-off-by: MJCoderMJCoder <598157378@qq.com>

---
+++ src/factoryPatterns/FactoryPatternDemo.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package factoryPatterns;
+
+import factoryPatterns.impl.Circle;
+import factoryPatterns.impl.Rectangle;
+import factoryPatterns.impl.Square;
+
+/**
+ * @author MJCoder
+ * 
+ *         4.使用该工厂，通过传递类型信息来获取实体类的对象。
+ */
+public class FactoryPatternDemo {
+
+	/**
+	 * 
+	 */
+	public FactoryPatternDemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		ShapeFactory shapeFactory = new ShapeFactory();
+
+		// 获取 Circle 的对象，并调用它的 draw 方法
+		Shape shape1 = shapeFactory.getShape("CIRCLE");
+		// 调用 Circle 的 draw 方法
+		shape1.draw();
+
+		// 获取 Rectangle 的对象，并调用它的 draw 方法
+		Shape shape2 = shapeFactory.getShape("RECTANGLE");
+		// 调用 Rectangle 的 draw 方法
+		shape2.draw();
+
+		// 获取 Square 的对象，并调用它的 draw 方法
+		Shape shape3 = shapeFactory.getShape("SQUARE");
+		// 调用 Square 的 draw 方法
+		shape3.draw();
+
+		// 使用反射机制可以解决每次增加一个产品时，都需要增加一个对象实现工厂的缺点
+		Rectangle rect = (Rectangle) ShapeFactory.getClass(Rectangle.class);
+		rect.draw();
+		Square square = (Square) ShapeFactory.getClass(Square.class);
+		square.draw();
+		Circle circle = (Circle) ShapeFactory.getClass(Circle.class);
+		circle.draw();
+	}
+
+}
---
+++ src/factoryPatterns/impl/Square.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package factoryPatterns.impl;
+
+import factoryPatterns.Shape;
+
+/**
+ * @author MJCoder
+ * 
+ *         2.创建实现接口的实体类。
+ * 
+ *         类在实现接口的方法时，不能抛出强制性异常，只能在接口中，或者继承接口的抽象类中抛出该强制性异常。
+ * 
+ *         类在重写方法时要保持一致的方法名，并且应该保持相同或者相兼容的返回值类型。
+ * 
+ *         如果实现接口的类是抽象类，那么就没必要实现该接口的方法。
+ *
+ */
+public class Square implements Shape {
+
+	/**
+	 * 
+	 */
+	public Square() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see factoryPatterns.Shape#draw()
+	 */
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Inside Square::draw() method.");
+	}
+
+}
