ad67a8f852055a5a8d6f37a9f4048178dd5e3e51
MJCoderMJCoder <598157378@qq.com>
2018-02-06 16:47:06 +0800

    设计模式：工厂模式、抽象工厂模式、单例模式、建造者模式、原型模式、适配器模式、桥接模式、过滤器模式、组合模式、装饰器模式、外观模式、享元模式、代理模式
    Signed-off-by: MJCoderMJCoder <598157378@qq.com>

---
+++ src/proxyPattern/ProxyPatternDemo.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package proxyPattern;
+
+import proxyPattern.image.Image;
+import proxyPattern.image.impl.ProxyImage;
+
+/**
+ * 当被请求时，使用 ProxyImage 来获取 RealImage 类的对象。
+ * 
+ * @author MJCoder
+ *
+ */
+public class ProxyPatternDemo {
+
+	/**
+	 * 
+	 */
+	public ProxyPatternDemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Image image = new ProxyImage("test_10mb.jpg");
+
+		// 图像将从磁盘加载
+		image.display();
+		System.out.println("");
+
+		// 图像将无法从磁盘加载
+		image.display();
+	}
+
+}
---
+++ src/proxyPattern/image/Image.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package proxyPattern.image;
+
+/**
+ * 创建一个接口。
+ * 
+ * @author MJCoder
+ *
+ */
+public interface Image {
+	void display();
+}
