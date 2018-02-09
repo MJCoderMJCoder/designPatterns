1c4fff924f9155844447cac85e24c336f6bb6aee
MJCoderMJCoder <598157378@qq.com>
2018-02-09 16:18:05 +0800

    Signed-off-by: MJCoderMJCoder <598157378@qq.com>

--- src/bridgePattern/shape/Shape.java
+++ src/bridgePattern/shape/Shape.java
@@ -6,8 +6,10 @@ package bridgePattern.shape;
 import bridgePattern.color.DrawAPI;
 
 /**
+ * 桥接
+ * 
  * @author MJCoder
- *
+ * 
  *         使用DrawAPI接口创建抽象类 Shape。
  */
 public abstract class Shape {
