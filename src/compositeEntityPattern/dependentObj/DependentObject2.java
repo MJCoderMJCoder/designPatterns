772b2b4a2dec2e2517ff83f754695acf44afc82a
MJCoderMJCoder <598157378@qq.com>
2018-02-11 19:26:25 +0800

    设计模式：工厂模式、抽象工厂模式、单例模式、建造者模式、原型模式；适配器模式、桥接模式、过滤器模式、组合模式、装饰器模式、外观模式、享元模式、代理模式；责任链模式、命令模式、解释器模式、迭代器模式、中介者模式、备忘录模式、观察者模式、状态模式、空对象模式、策略模式、模板模式、访问者模式；MVC模式、务代表模式、组合实体模式
    Signed-off-by: MJCoderMJCoder <598157378@qq.com>

---
+++ src/compositeEntityPattern/dependentObj/DependentObject2.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package compositeEntityPattern.dependentObj;
+
+/**
+ * 创建依赖对象。
+ * 
+ * @author MJCoder
+ *
+ */
+public class DependentObject2 {
+	private String data;
+
+	/**
+	 * 
+	 */
+	public DependentObject2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @return the data
+	 */
+	public String getData() {
+		return data;
+	}
+
+	/**
+	 * @param data
+	 *            the data to set
+	 */
+	public void setData(String data) {
+		this.data = data;
+	}
+
+}
