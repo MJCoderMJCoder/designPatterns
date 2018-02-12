7395e0de1f1bfcbe45844dd088a80d4826a42ec3
MJCoderMJCoder <598157378@qq.com>
2018-02-12 10:22:10 +0800

    设计模式：工厂模式、抽象工厂模式、单例模式、建造者模式、原型模式；适配器模式、桥接模式、过滤器模式、组合模式、装饰器模式、外观模式、享元模式、代理模式；责任链模式、命令模式、解释器模式、迭代器模式、中介者模式、备忘录模式、观察者模式、状态模式、空对象模式、策略模式、模板模式、访问者模式；MVC模式、业务代表模式、组合实体模式、数据访问对象模式、前端控制器模式、拦截过滤器模式

---
+++ src/interceptingFilterPattern/InterceptingFilterDemo.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package interceptingFilterPattern;
+
+import interceptingFilterPattern.filter.impl.AuthenticationFilter;
+import interceptingFilterPattern.filter.impl.DebugFilter;
+
+/**
+ * 使用 Client 来演示拦截过滤器设计模式。
+ * 
+ * @author MJCoder
+ *
+ */
+public class InterceptingFilterDemo {
+
+	/**
+	 * 
+	 */
+	public InterceptingFilterDemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		FilterManager filterManager = new FilterManager(new Target());
+		filterManager.setFilter(new AuthenticationFilter());
+		filterManager.setFilter(new DebugFilter());
+
+		Client client = new Client();
+		client.setFilterManager(filterManager);
+		client.sendRequest("HOME");
+	}
+}
---
+++ src/interceptingFilterPattern/filter/Filter.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package interceptingFilterPattern.filter;
+
+/**
+ * 创建过滤器接口 Filter。
+ * 
+ * @author MJCoder
+ *
+ */
+public interface Filter {
+	public void execute(String request);
+}
